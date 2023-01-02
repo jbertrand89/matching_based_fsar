fbc629193a903962198a2ca13a43635227435119
Juliette Bertrand <juliette.ld.bertrand@gmail.com>
2023-01-02 15:50:10 +0100

    remove old code cum_dist

--- src/few_shot_models/otam_model.py
+++ src/few_shot_models/otam_model.py
@@ -8,44 +8,6 @@ from src.matching_functions.visil_fcn import VisilFCN
 from .few_shot_head import CNN_FSHead
 
 
-def OTAM_cum_dist(dists, lbda=0.1):
-    """
-    Calculates the OTAM distances for sequences in one direction (e.g. query to support).
-    :input: Tensor with frame similarity scores of shape [n_queries, n_support, query_seq_len, support_seq_len]
-    TODO: clearn up if possible - currently messy to work with pt1.8. Possibly due to stack operation?
-    """
-    dists = F.pad(dists, (1, 1), 'constant', 0)
-
-    cum_dists = torch.zeros(dists.shape, device=dists.device)
-
-    # top row
-    for m in range(1, dists.shape[3]):
-        # cum_dists[:,:,0,m] = dists[:,:,0,m] - lbda * torch.log( torch.exp(- cum_dists[:,:,0,m-1]))
-        # paper does continuous relaxation of the cum_dists entry, but it trains faster without, so using the simpler version for now:
-        cum_dists[:, :, 0, m] = dists[:, :, 0, m] + cum_dists[:, :, 0, m - 1]
-
-        # remaining rows
-    for l in range(1, dists.shape[2]):
-        # first non-zero column
-        cum_dists[:, :, l, 1] = dists[:, :, l, 1] - lbda * torch.log(
-            torch.exp(- cum_dists[:, :, l - 1, 0] / lbda) + torch.exp(
-                - cum_dists[:, :, l - 1, 1] / lbda) + torch.exp(- cum_dists[:, :, l, 0] / lbda))
-
-        # middle columns
-        for m in range(2, dists.shape[3] - 1):
-            cum_dists[:, :, l, m] = dists[:, :, l, m] - lbda * torch.log(
-                torch.exp(- cum_dists[:, :, l - 1, m - 1] / lbda) + torch.exp(
-                    - cum_dists[:, :, l, m - 1] / lbda))
-
-        # last column
-        # cum_dists[:,:,l,-1] = dists[:,:,l,-1] - lbda * torch.log( torch.exp(- cum_dists[:,:,l-1,-2] / lbda) + torch.exp(- cum_dists[:,:,l,-2] / lbda) )
-        cum_dists[:, :, l, -1] = dists[:, :, l, -1] - lbda * torch.log(
-            torch.exp(- cum_dists[:, :, l - 1, -2] / lbda) + torch.exp(
-                - cum_dists[:, :, l - 1, -1] / lbda) + torch.exp(- cum_dists[:, :, l, -2] / lbda))
-
-    return cum_dists[:, :, -1, -1]
-
-
 class CNN_OTAM(CNN_FSHead):
     """
     OTAM with a CNN backbone.
